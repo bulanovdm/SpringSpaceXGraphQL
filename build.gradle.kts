949800625593e7b44404db0df0d8add1d551dc4e
Bulanov Dmitriy <bulanovdms@gmail.com>
2022-09-04 21:33:23 +0300

    add generated files by Apollo to build

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,24 +14,28 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
+	flatDir {
+		dirs ("build/generated/source/apollo/service/com/bulanovdm/ssgql/graphql")
+	}
 }
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-graphql")
-	implementation("org.springframework.boot:spring-boot-starter-mustache")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation ("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("com.graphql-java:graphql-java-extended-scalars:18.1")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	implementation("com.graphql-java:graphql-java-extended-scalars:18.1")
 	implementation("com.apollographql.apollo3:apollo-runtime:3.5.0")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.springframework:spring-webflux")
 	testImplementation("org.springframework.graphql:spring-graphql-test")
+	testImplementation ("io.projectreactor:reactor-test")
 }
 
 apollo {
